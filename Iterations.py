a72aa4b39f2a914af3500d997d8c14d8610c07fd
coffeeGhost-rj <riteshjangra2874@gmail.com>
2026-07-28 10:05:06 +0530

    files changes

--- Iterations.py
+++ Iterations.py
@@ -1,6 +1,6 @@
-Nostart = int(input("Enter the starting number: "))
-Nend = int(input("Enter the ending number: "))
-multiplier = int(input("Enter the multiplier: "))   
+# Nostart = int(input("Enter the starting number: "))
+# Nend = int(input("Enter the ending number: "))
+# multiplier = int(input("Enter the multiplier: "))   
 
 # do-while loop to print the multiples of the multiplier from Nostart to Nend
 # while True:
@@ -11,6 +11,24 @@ multiplier = int(input("Enter the multiplier: "))
 #     if choice.lower() != 'y':
 #         break
 
-for i in range(Nostart, Nend + 1):
-    if i % multiplier == 0:
-        print(i)
+# for i in range(Nostart, Nend + 1):
+#     if i % multiplier == 0:
+#         print(i)
+
+
+
+number = int(input("Enter a number to check:"))
+
+if number < 0:
+    print("Negative numbers.")
+elif number == 0:
+    print("Zero.")
+elif number > 0:
+    print("Positive numbers.")
+    
+if number == 0:
+    print("Zero.")
+elif number % 2 == 0 and number != 0:
+    print("Even number.")
+else:
+    print("Odd number.")
